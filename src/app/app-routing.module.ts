77fd1b5e5ac7c7371a6744ec388b7d114ceb58d2
lwill2897 <51091301+lwill2897@users.noreply.github.com>
2020-11-27 13:18:54 -0600

    Arreglando el menu revisando otros componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
+import { LoginComponent } from './login/login.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { primariaComponent } from './primaria/primaria.component';
-import { IniciosesionComponent } from './iniciosesion/iniciosesion.component';
+import { from } from 'rxjs';
 
 const routes: Routes = [
   {path:'Inicio', component: InicioComponent},
-  {path:'**', redirectTo:'Inicio', pathMatch:'full'},
+  {path:'', redirectTo:'Inicio', pathMatch:'full'},
   {path:'Primaria', component: primariaComponent},
-  {path:'**', redirectTo:'Primaria', pathMatch:'full'},
-  {path:'Iniciosesion', component: IniciosesionComponent},
-  {path:'**', redirectTo:'Iniciosesion', pathMatch:'full'}
+  {path:'', redirectTo:'Primaria', pathMatch:'full'},
+  {path:'Login', component: LoginComponent},
+  {path:'', redirectTo:'Login', pathMatch:'full'}
 ];
 
 @NgModule({
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { IniciosesionComponent } from './iniciosesion.component';
+import { LoginComponent } from './login.component';
 
-describe('IniciosesionComponent', () => {
-  let component: IniciosesionComponent;
-  let fixture: ComponentFixture<IniciosesionComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ IniciosesionComponent ]
+      declarations: [ LoginComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(IniciosesionComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
