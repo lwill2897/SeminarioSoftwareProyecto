fd89d85f22fd8ad1192411ba44cd18d823320a12
lwill2897 <51091301+lwill2897@users.noreply.github.com>
2020-12-02 15:45:08 -0600

    Agregando firebase al proyecto

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCV_OxXbI2c6bvX34pGReys7SuG8uWfSU0",
+    authDomain: "libreria-ae1a9.firebaseapp.com",
+    databaseURL: "https://libreria-ae1a9.firebaseio.com",
+    projectId: "libreria-ae1a9",
+    storageBucket: "libreria-ae1a9.appspot.com",
+    messagingSenderId: "929336323258",
+    appId: "1:929336323258:web:58efd1f3f78d4788f7a683",
+    measurementId: "G-1LHT2DP09Y"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCV_OxXbI2c6bvX34pGReys7SuG8uWfSU0",
+    authDomain: "libreria-ae1a9.firebaseapp.com",
+    databaseURL: "https://libreria-ae1a9.firebaseio.com",
+    projectId: "libreria-ae1a9",
+    storageBucket: "libreria-ae1a9.appspot.com",
+    messagingSenderId: "929336323258",
+    appId: "1:929336323258:web:58efd1f3f78d4788f7a683",
+    measurementId: "G-1LHT2DP09Y"
+  }
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
